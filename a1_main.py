88674fa93e659530f01566aec6d56153be7de302
Oscar Coleman <161176613+oscarcoleman@users.noreply.github.com>
2025-05-27 12:27:37 +1000

    27/5

--- a1_main.py
+++ a1_main.py
@@ -20,3 +20,11 @@ print(df.describe())
 # Shows country codes (of athletes participating)
 print(df['NOC'].nunique())
 print(df['NOC'].unique())
+
+# Filter for female athletes only
+female_athletes = df[df['Sex'] == 'F']
+print(female_athletes.head())
+
+# Filter for athletes older than 35
+older_athletes = df[df['Age'] > 35]
+print(older_athletes[['Name', 'Age', 'Sport']].head())
