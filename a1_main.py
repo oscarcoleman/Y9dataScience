7e4fad71042cf6542d3392ec5e4e89273bfe0c9b
Oscar Coleman <161176613+oscarcoleman@users.noreply.github.com>
2025-05-27 10:38:21 +1000

    27/5

--- a1_main.py
+++ a1_main.py
@@ -8,3 +8,15 @@ print(df.head())
 # Show the column names
 print(df.columns)
 
+# Shows top sports
+print(df['Sport'].value_counts().head())
+
+# Shows number of male vs female athletes
+print(df['Sex'].value_counts())
+
+# Shows stats such as mean and min
+print(df.describe())
+
+# Shows country codes (of athletes participating)
+print(df['NOC'].nunique())
+print(df['NOC'].unique())
