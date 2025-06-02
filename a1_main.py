79836b8986166dd3d917d459e6f0a118bd0bdef7
Oscar Coleman <161176613+oscarcoleman@users.noreply.github.com>
2025-06-02 14:30:46 +1000

    27/5

--- a1_main.py
+++ a1_main.py
@@ -27,4 +27,4 @@ print(female_athletes.head())
 
 # Filter for athletes older than 35
 older_athletes = df[df['Age'] > 35]
-print(older_athletes[['Name', 'Age', 'Sport']].head())
+print(len(older_athletes[['Name', 'Age', 'Sport']].head()))
