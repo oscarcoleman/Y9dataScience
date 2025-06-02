5d89e8124466a4995981117f3c585159a9dac117
Oscar Coleman <161176613+oscarcoleman@users.noreply.github.com>
2025-06-02 14:28:15 +1000

    Update a1_main.py

--- a1_main.py
+++ a1_main.py
@@ -28,3 +28,19 @@ print(female_athletes.head())
 # Filter for athletes older than 35
 older_athletes = df[df['Age'] > 35]
 print(older_athletes[['Name', 'Age', 'Sport']].head())
+
+# Filter for athletes from Australia in Swimming
+australian_swimmers = df[(df['Country'] == 'Australia') & (df['Sport'] == 'Swimming')]
+print(australian_swimmers.head())
+
+# Sort by height
+sorted_by_height = df.sort_values(by='Height', ascending=False)
+print(sorted_by_age[['Name', 'Height', 'Sport']].head(10))
+
+# Sort by weight
+sorted_by_weight = df.sort_values(by='Weight', ascending=False)
+print(sorted_by_weight[['Name', 'Weight', 'Sport']].head(10))
+
+# Count participants in each sport
+sport_counts = female_athletes['Sport'].value_counts()
+print(sport_counts.head())
